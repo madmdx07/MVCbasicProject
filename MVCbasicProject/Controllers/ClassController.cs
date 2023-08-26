eb79310ba10a4e5513e7b9fe02eeba983d173db1
madmdx07 <mahessdhant@gmail.com>
2023-08-26 11:13:42 +0545

    Add project files.

---
+++ MVCbasicProject/Controllers/ClassController.cs
@@ -0,0 +1,20 @@
+﻿using MVCbasicProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCbasicProject.Controllers
+{
+    public class ClassController : Controller
+    {
+        // GET: Class
+        public ActionResult Index()
+        {
+            StudentDbEntities db = new StudentDbEntities();
+            List<tblClass> classes = db.tblClasses.ToList();
+            return View(classes);
+        }
+    }
+}
---
+++ MVCbasicProject/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using MVCbasicProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCbasicProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDbEntities db = new StudentDbEntities();
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<tblStudent> students = db.tblStudents.ToList();
+            return View(students);
+        }
+
+        public ActionResult ClassStudents(int cId)
+        {
+            List<tblStudent> students = db.tblStudents.Where(id => id.Grade == cId).ToList();
+            return View(students);
+        }
+    }
+}
